7ea6d6b6fb4df0405b84d319a0d54cf0ee644372
SaiPranith <saipranithswargam@gmail.com>
2022-08-26 22:02:40 +0530

    project under process

---
+++ app.js
@@ -0,0 +1,52 @@
+const express = require('express');
+
+const bodyParser = require('body-parser');
+
+const ejs = require('ejs');
+
+require('dotenv').config();
+
+
+const apikey = process.env.API_KEY;
+
+const app = express();
+
+const axios = require('axios');
+
+app.use(bodyParser.urlencoded({extended:true}));
+
+app.set('view engine','ejs');
+
+app.use(express.static("public"));
+
+
+app.get('/',(req,res)=>{
+    res.sendFile(__dirname+"/index.html");
+})
+
+app.post("/",(req,res)=>{
+    axios.get(`https://www.alphavantage.co/query?function=TIME_SERIES_WEEKLY_ADJUSTED&symbol=IBM&apikey=${apikey}`).then((response)=>{
+        const d = response.data;
+        const k = Object.keys(d);
+        const l = response.data[k[1]];
+        const m =Object.keys(l);
+        const obj = l[m[1]];
+        const kes = Object.keys(obj);
+        const vals =[];
+        kes.forEach(element => {
+            vals.push(obj[element]);
+        });
+        res.sendFile(__dirname+"/query.html");
+    })
+    })
+
+app.get("/res",(req,res)=>{
+    res.sendFile(__dirname+"/res.html");
+})
+
+app.post("/res",(req,res)=>{
+   
+})
+app.listen(process.env.PORT||3000,()=>{
+    console.log('listening to port 3000');
+})
